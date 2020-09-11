14d1dbcfe5721986df24fe39e1d55663b4261258
Dhananjay Sisode <dhananjay_sisode@persistent.co.in>
2020-09-11 17:56:32 +0530

    project setup and first working burger builder image

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import Aux from '../../hoc/Auxillary'
+import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+
+class BurgerBuilder extends Component{
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 2 
+        }
+    }
+
+    addIngredientHandler = (type) => {
+
+    }
+
+    removeIngredientHandler = (type) => {
+
+    }
+    
+    render() {
+        return(
+            <Aux>
+                <Burger ingredients={this.state.ingredients}></Burger>
+                <BuildControls />
+            </Aux>
+        );
+    }
+}
+
+export default BurgerBuilder;
